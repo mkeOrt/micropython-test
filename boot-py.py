6b273709a1040c288567d11937be6b63b5d2e12b
Miguel Ortega <mke@192.168.1.8>
2023-01-18 20:53:22 -0600

    change speed

--- boot-py.py
+++ boot-py.py
@@ -1,3 +1,15 @@
+import machine
+import time
+
+led = machine.Pin(2,machine.Pin.OUT)
+
+
+for True:
+    led.value(1)
+    time.sleep(.2)
+    led.value(0)
+    time.sleep(.1)
+
 
 import ugit
 ugit.pull_all()
