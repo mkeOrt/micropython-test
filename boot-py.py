9b46dbb01fa8d82eb2de6b734d5e0bbda6f6278d
Mike <mke.ort@gmail.com>
2023-01-18 20:57:07 -0600

    change to 5 sec

--- boot-py.py
+++ boot-py.py
@@ -6,9 +6,9 @@ led = machine.Pin(2,machine.Pin.OUT)
 
 while True:
     led.value(1)
-    time.sleep_ms(500)
+    time.sleep_ms(5000)
     led.value(0)
-    time.sleep_ms(500)
+    time.sleep_ms(5000)
 
 
 import ugit
